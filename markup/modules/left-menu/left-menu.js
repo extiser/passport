4594a6abc220b5b63ac9834bebd9f85707288cce
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-07-30 18:05:13 +0500

    fix

--- markup/modules/left-menu/left-menu.js
+++ markup/modules/left-menu/left-menu.js
@@ -2,10 +2,9 @@ jQuery(document).ready(function ($) {
     $('.left-menu__mobile').click(function () {
         $('.sidebar-menu, body, .mask, .left-menu__mobile').toggleClass('active');
         var documentHeight = $(document).height();
-        alert(documentHeight);
-
         var windowHeight = $(window).height();
-        alert(windowHeight);
+
+        $('.sidebar-menu').height(documentHeight);
     });
     $('.mask').click(function () {
         $('.sidebar-menu, body, .mask, .left-menu__mobile').removeClass('active');
