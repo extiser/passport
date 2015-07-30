d209efe52a4bb54621bcb6bf54f47a25adf49720
System Administrator <root@MacBook-Pro-Ruslan.local>
2015-07-30 21:26:10 +0500

    finish work

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,26 +1,3 @@
-/*$(window).scroll(function () {
-    var top = $(document).scrollTop();
-    if (top > 0) {
-        $('.left-menu__mobile').addClass('fixed');
-    } else {
-        $('.left-menu__mobile').removeClass('fixed');
-    }
-});
-
-function onScroll() {
-    if ($('.left-menu__mobile').hasClass('active')) {
-        $(window).unbind('scroll');
-    } else {
-        var top = $(document).scrollTop();
-        if (top > 0) {
-            $('.left-menu__mobile').addClass('fixed');
-        } else {
-            $('.left-menu__mobile').removeClass('fixed');
-        }
-    }
-}
-
-$(window).scroll(onScroll);*/
 jQuery(document).ready(function ($) {
     $(window).load(function () {
         $('.sidebar-menu').mCustomScrollbar({
