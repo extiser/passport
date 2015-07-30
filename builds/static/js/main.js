cd33fefd5663c4597af886fdf477ff8cd4437cbb
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-07-30 12:44:35 +0500

    build

---
+++ builds/static/js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+    $('.left-menu__mobile').click(function () {
+
+        var menu = $('.left-menu');
+
+        if ($(this).hasClass('active')) {
+            $('.menu-slide').detach();
+            menu.clone().appendTo('.sidebar');
+            $('.main__slide, .menu-slide, .left-menu__mobile').removeClass('active');
+        } else {
+            $('.header').after('<div class="menu-slide"></div>');
+
+            menu.clone().prependTo('.menu-slide');
+
+            $('.sidebar .left-menu').detach();
+
+            $('.main__slide, .menu-slide, .left-menu__mobile').addClass('active');
+        }
+    });
+});
